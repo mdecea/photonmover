fdaa6db6116ca3d4f8256415324e2b50da36d3c0
Gavin West <westgn@mit.edu>
2022-10-17 17:46:40 -0400

    Fixing many typos and bugs, adding small functionality to HP VSA

--- photonmover/instruments/Vector_signal_analyzers/HP89410A.py
+++ photonmover/instruments/Vector_signal_analyzers/HP89410A.py
@@ -4,17 +4,15 @@ from photonmover.Interfaces.Instrument import Instrument
 import time
 import pyvisa as visa
 
-GPIB_ADDR = "GPIB1::19::INSTR"  # GPIB adress
-
 
 class HP89410A(Instrument):
     """
     Code for controlling the HP89410A vector signal analyzer
     """
-
-    def __init__(self):
+    def __init__(self, gpib_address=None):
         super().__init__()
         # It is good practice to initialize variables in init
+        self.gpib_address = gpib_address
         self.gpib = None
 
     def initialize(self):
@@ -22,14 +20,18 @@ class HP89410A(Instrument):
         Initializes the instrument
         :return:
         """
-        print('Opening connnection to HP89410A vector signal analyzer')
 
-        rm = visa.ResourceManager()
-        try:
-            self.gpib = rm.open_resource(GPIB_ADDR, timeout=5000)
-        except BaseException:
-            raise ValueError(
-                'Cannot connect to NeHP89410A vector signal analyzer')
+        if self.gpib_address is not None:
+            print('Opening connnection to HP89410A vector signal analyzer')
+
+            rm = visa.ResourceManager()
+            try:
+                self.gpib = rm.open_resource(self.gpib_address, timeout=5000)
+            except BaseException:
+                raise ValueError(
+                    'Cannot connect to HP89410A vector signal analyzer')
+        else:
+            print("No GPIB address provided, please re-instantiate with an address")
 
     def close(self):
         print('Disconnecting HP89410A vector signal analyzer')
@@ -79,6 +81,29 @@ class HP89410A(Instrument):
 
         self.gpib.write('INP%d:STAT OFF;' % channel)
 
+    def set_freq_axis(self, center, span, start_freq, end_freq):
+        """
+        Sets the center and span frequencies, if specified, or
+        the start and end frequencies.
+        :param center: string with units or in Hz. Example: '700 MHZ' or '700e6'
+        :param span: string with units. Example:  '100 MHZ' or '100e6'
+        :param start_freq: initial frequency (string with units)
+        :param end_freq: end frequency (string with units)
+        :return:
+        """
+
+        if center is not None:
+            self.gpib.write('SENS:FREQ:CENT %s;' % center)
+
+        if span is not None:
+            self.gpib.write('SENS:FREQ:SPAN %s;' % span)
+
+        if start_freq is not None:
+            self.gpib.write('SENS:FREQ:STAR %s;' % start_freq)
+
+        if end_freq is not None:
+            self.gpib.write('SENS:FREQ:STOP %s;' % end_freq)
+
     def set_y_unit(self, unit):
         """
         Specifes the y axis unit
@@ -121,6 +146,13 @@ class HP89410A(Instrument):
 
     def autoscale_y(self):
         self.gpib.write('DISP:WIND:TRAC:Y:AUTO ONCE;')
+
+    def get_rbw(self):
+        """
+        Returns the resolution bandwidth setting, in Hz.
+        """
+        rbw = self.gpib.query_ascii_values('SENS:BAND:RES?')[0]
+        return rbw
 
     def set_rbw(self, rbw):
         """
@@ -171,17 +203,17 @@ class HP89410A(Instrument):
             print("Data register number not correct. Doing nothing.")
             return
 
-        print(data_reg_num)
+        # print(data_reg_num)
         # self.gpib.write('TRACE:COPY D%d, TRAC%d' % (data_reg_num, trace_num))
-        self.gpib.write('SYST:KEY 24;')
-        self.gpib.write('SYST:KEY 111;')
-        self.gpib.write('SYST:KEY %d;' % (111 + data_reg_num))
+        self.gpib.write('SYST:KEY 24;')  # KEY: Save/recall
+        self.gpib.write('SYST:KEY 111;')  # KEY: F1
+        self.gpib.write('SYST:KEY %d;' % (111 + data_reg_num))  # KEY: Shifted
 
-    def retrieve_data(self, trace_id, filename):
+    def retrieve_data(self, trace_id, filename=None):
         """
         Retrieves the data saved in the instrument memory
         :param trace_id: id of the trace. 1 - 6.
-        :param filename: filename where data should be saved
+        :param filename: Optional; filename in which to store data [csv format].
         :return:
         """
 
@@ -189,34 +221,54 @@ class HP89410A(Instrument):
             print("Trace ID not correct. Doing nothing.")
             return
 
-        xdata = self.gpib.query("TRACE:X:DATA? D%d" % trace_id)
-        ydata = self.gpib.query("TRACE:DATA? D%d" % trace_id)
+        xdata = self.gpib.query("TRACE:X:DATA? D{:d}".format(trace_id))
+        ydata = self.gpib.query("TRACE:DATA? D{:d}".format(trace_id))
 
         xdata = np.array(xdata.split(','), dtype=float)
         ydata = np.array(ydata.split(','), dtype=float)
 
-        # print(xdata[0])
-        # print(xdata[-1])
-        # input()
-
         # convert to V/sqrt(Hz)
         ydata = np.sqrt(ydata) / np.sqrt(2)
 
-        with open(filename + '.csv', 'w+') as csvfile:
-            writer = csv.writer(csvfile)
-            writer.writerow(xdata)
-            writer.writerow(ydata)
+        if filename is not None:
+            with open(filename + '.csv', 'w+') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(xdata)
+                writer.writerow(ydata)
 
-    def perform_freq_noise_experiment(
-            self, freqs, num_averages, rbws, base_filename):
+        return (xdata, ydata)
+
+    def convert_to_noise_spectra(self, freqs, Sv, Vpp_interferometer, dL):
         """
-        Performs a frequency noise experiment. We need to write a script because the
-        VSA can only take spectra with linearly spaced points. Therefore, to get
-        a higher resolution at low frequencies, what we do is take several spectrums
-        over the whole frequency range we want to cover.
+        Converts the vsa data into frequency noise spectra, assuming the noise 
+        was measured using an imbalanced MZI approach. 
 
-        We will take one spectrum between freqs[0] and freqs[1]; another between freqs[1]
-        and freqs[2], ...
+        :param freqs: vector of frequencies (in Hz)
+        :param Sv: voltage noise masured by the VSA (in V/sqrt(Hz))
+        :param Vpp_interferometer: peak to peak voltage of the interferometer (Vmax - Vmin).
+        :param dL: path length between the two arms of the interferometer
+        """
+
+        c = 2.99e8
+        neff = 1.48
+
+        dV = Vpp_interferometer / 2
+        tau = dL / (c / neff)
+
+        denom = dV / np.sqrt(2) * 2 * np.pi * tau * np.sinc(freqs * tau)
+
+        Sf = (Sv / denom) ** 2
+
+        return [freqs, Sf]
+
+    def collect_signal_spectrum(self, freqs, num_averages, rbws, filename=None):
+        """
+        Collects a large spectrum signal.
+        The VSA is limited to taking data on a linearly-spaced frequency grid.
+        Over broad bandwidths this leads to unnecessarily coarse or fine spacings
+        on the extreme ends.
+        In order to get a balanced, quasi-frequency-scaled resolution, we take
+        several spectra over the whole frequency range we want to cover.
 
         :param freqs: list with the edge frequencies of the different slices. We will take one
             spectrum between freqs[0] and freqs[1]; another between freqs[1] and freqs[2], ...
@@ -226,7 +278,7 @@ class HP89410A(Instrument):
         :param rbws: resolution bandwidth to use. It can be a single number (and then all
             frequency slices will have the same rbw) or a vector with the length len(freqs-1).
             In this latter case, each slice will use the specified rbw.
-        :param base_filename: start of the filename where the data will be saved. The data register number will be appended.
+        :param filename: Optional; Stores output data in `<filename>.csv`.
         """
 
         self.set_averaging(0, 'RMS', None)
@@ -244,109 +296,72 @@ class HP89410A(Instrument):
             change_rbw = 0
             self.set_rbw(rbws)
 
-        for i in range(len(freqs) - 1):
-
+        for idx in range(len(freqs) - 1):
             # Set axis
-            self.set_freq_axis(None, None, freqs[0], freqs[i + 1])
-
-            # Autoscale
+            self.set_freq_axis(None, None, freqs[idx], freqs[idx + 1])
             self.autoscale_y()
 
             # Set rbw if necessary
             if change_rbw:
-                self.set_rbw(rbws[i])
+                self.set_rbw(rbws[idx])
 
             # Set averaging if necessary
             if change_aver:
-                self.set_averaging(1, 'RMS', num_averages[i])
-
-                # Start measuring
-                vsa.gpib.write('SYST:KEY 21;')
+                self.set_averaging(1, 'RMS', num_averages[idx])
+                self.gpib.write('SYST:KEY 21;')  # KEY: Meas Restart
 
                 time.sleep(20)
                 # Wait for measurement
-                while self.averages_taken() < num_averages[i]:
+                while self.averages_taken() < num_averages[idx]:
                     print('Waiting')
                     time.sleep(10)
 
             else:
                 # Start measuring
-                vsa.gpib.write('SYST:KEY 21;')
+                self.gpib.write('SYST:KEY 21;')  # KEY: Meas Restart
 
                 # Wait until done
-                time.sleep(20)
+                time.sleep(10)
                 while self.averages_taken() < num_averages:
-                    time.sleep(10)
+                    time.sleep(5)
 
             # Save data into data register
-            self.save_trace_to_memory(1, i + 1)
+            self.save_trace_to_memory(1, idx + 1)
             time.sleep(1)
 
         # After all is done, save the register data into files
-        for i in range(len(freqs) - 1):
-            filename = base_filename + '_' + str(i)
-            self.retrieve_data(i + 1, filename)
+        freq_regs = []  # initialize lists for frequency, signal data
+        sig_regs = []
+        for idx in range(len(freqs) - 1):
+            data_register = idx+1
+            xd, yd = self.retrieve_data(data_register)
 
-    def set_freq_axis(self, center, span, start_freq, end_freq):
-        """
-        Sets the center and span frequencies, if specified, or
-        the start and end frequencies.
-        :param center: string with units or in Hz. Example: '700 MHZ' or '700e6'
-        :param span: string with units. Example:  '100 MHZ' or '100e6'
-        :param start_freq: initial frequency (string with units)
-        :param end_freq: end frequency (string with units)
-        :return:
-        """
+            # returned data is sometimes outside the specified frequency bounds
+            # Find closest frequencies and slice
+            min_indx = np.abs(xd-freqs[idx]).argmin()
+            max_indx = np.abs(xd-freqs[idx+1]).argmin()+1
+            xd, yd = xd[min_indx:max_indx], yd[min_indx:max_indx]
 
-        if center is not None:
-            self.gpib.write('SENS:FREQ:CENT %s;' % center)
+            freq_regs.append(xd)
+            sig_regs.append(yd)
 
-        if span is not None:
-            self.gpib.write('SENS:FREQ:SPAN %s;' % span)
+        freq = np.concatenate(freq_regs, axis=-1)
+        sig = np.concatenate(sig_regs, axis=-1)
 
-        if start_freq is not None:
-            self.gpib.write('SENS:FREQ:STAR %s;' % start_freq)
+        if filename is not None:
+            with open(filename + '.csv', 'w+') as csvfile:
+                writer = csv.writer(csvfile)
+                writer.writerow(freq)
+                writer.writerow(sig)
 
-        if end_freq is not None:
-            self.gpib.write('SENS:FREQ:STOP %s;' % end_freq)
-
-    def convert_to_noise_spectra(self, freqs, Sv, Vpp_interferometer, dL):
-        """
-        Converts the vsa data into frequency noise spectra.
-        :param freqs: vector of frequencies (in Hz)
-        :param sV: voltage noise masured by the VSA (in V/sqrt(Hz))
-        :param Vpp_interferometer: peak to peak voltage of the interferometer (Vmax - Vmin).
-        :param dL: path length between the two arms of the interferometer
-        """
-
-        c = 2.99e8
-        neff = 1.48
-
-        dV = Vpp_interferometer / 2
-        tau = dL / (c / neff)
-
-        denom = dV / np.sqrt(2) * 2 * np.pi * tau * np.sinc(freqs * tau)
-
-        Sf = (Sv / denom) ** 2
-
-        return [freqs, Sf]
+        return freq, sig
 
 
 if __name__ == '__main__':
     vsa = HP89410A()
     vsa.initialize()
-    # vsa.set_freq_axis(None, None, '1', '100')
-    # vsa.set_rbw('3')
-    # print(vsa.averages_taken())
-    vsa.perform_freq_noise_experiment(['1', '100', '1000', '10000', '100e3', '1e6', '10e6'],
-                                      # [40, 40, 100, 500, 500, 500],
-                                      [600, 600, 200, 500, 500, 500],
-                                      ['1', '10', '50', '500', '5000', '50000'],
-                                      'HP_1550_vib_isol')
-    #vsa.gpib.write('FORM:DATA ASCII; ')
-    #xdata = vsa.gpib.query_ascii_values("TRACE:X:DATA? D%d; " % 1)
-    # print(xdata[1])
-    # print(vsa.gpib.query('TRAC:X:UNIT?;'))
-    #vsa.retrieve_data(1, 'a')
-    #vsa.retrieve_data(2, 'b')
+    vsa.collect_signal_spectrum(freqs=['1', '100', '1000', '10000', '100e3', '1e6', '10e6'],
+                                num_averages=[600, 600, 200, 500, 500, 500],
+                                rbws=['1', '10', '50', '500', '5000', '50000'],
+                                filename='HP_1550_vib_isol')
     vsa.close()
