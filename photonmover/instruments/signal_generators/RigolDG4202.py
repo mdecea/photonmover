f00e35d753c24f27081a484621143c385a1964ef
Gavin West <westgn@mit.edu>
2023-02-17 17:06:30 -0500

    Adding basic commands

--- photonmover/instruments/signal_generators/RigolDG4202.py
+++ photonmover/instruments/signal_generators/RigolDG4202.py
@@ -94,18 +94,90 @@ class RigolDG4202(Instrument):
         else:
             print("Cannot assign a new address to an initialized instrument.")
 
-    def set_channel_output_state(self, state='off', channels=[1,2]):
+    def set_channel_output_state(self, state='off', channel=[1,2]):
         """
         Set the state of the channel output 
         """
-
-
+        if not hasattr(channel, '__iter__'):
+            channel=[channel]
+        
+        for chan in channel:
+            state = state.upper()
+            self.instr.write(":OUTPut{:d}:STATe {:s}".format(chan, state))
 
     def get_channel_output_state(self):
         """
-        Checks what the output state of each channel is. 
-        Returns list of booleans.
+        Returns the output state for BOTH channels 
+        
         """
+        states={}
+        for chan in ["1","2"]:
+            states[chan] = self.instr.query(":OUTPut{:s}:STATe?".format(chan)).rstrip('\n')
+        
+        return states
+    
+    def set_voltage_amplitude(self, voltage, channel):
+        """
+        Sets the signal amplitude of the specified 
+        channel. 
+        Only works on a single channel at a time. 
+        """
+        self.instr.write(":SOURce{:d}:VOLTage:AMPLitude {:f}".format(channel, voltage))  
+    
+    def get_voltage_amplitude(self, channel): 
+        """
+        Queries the set voltage of the specified
+        channel. 
+        """
+        return self.instr.query(":SOURce{:d}:VOLTage:AMPLitude?".format(channel)).rstrip('\n')
+
+    def set_frequency(self, freq, channel):
+        """
+        Set the frequency of the output signal
+        
+        * **freq** (float): Frequency in Hz. 
+        """
+        if not hasattr(channel, '__iter__'):
+            channel=[channel]
+        
+        for chan in channel:
+            self.instr.write(":SOURce{:d}:FREQuency {:f}".format(chan, freq))
+
+    def get_frequency(self):
+        """
+        Get the frequency of the output signal on
+        both channels. 
+
+        Returns dict of set frequencies. 
+        """
+        
+        freqs = {}
+        for chan in ["1","2"]:
+            freqs[chan] = self.instr.query(":SOURce{:s}:FREQuency?".format(chan)).rstrip('\n')
+        return freqs 
+
+    def set_output_impedance(self, impedance, channel):
+        """
+        Sets the output impedance for the specified channel. 
+
+        * **channel** (int or list of ints): Channel number. 
+                        Can be an int or a list of ints, e.g. 
+                        channel=1 or channel=[1,2].
+                        All channels are given the same impedance. 
+        * **impedance** (multiple): Desired impedance value. Can
+                        be 50 or "infinity" for high Z output. 
+        """
+
+        if not hasattr(channel, '__iter__'):
+            channel=[channel]
+        
+        for chan in channel:
+            if str(impedance) == "50":
+                imp = "50"
+            elif impedance.lower() == "infinity":
+                imp = "INFinity" 
+
+            self.instr.write(":OUTPut{:d}:IMPedance {:s}".format(chan, imp))
 
 
 if __name__ == '__main__':
