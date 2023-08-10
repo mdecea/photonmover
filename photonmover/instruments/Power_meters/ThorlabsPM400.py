943b227facd956548c1aa7502c6c984800c341d4
Gavin West <westgn@mit.edu>
2023-08-10 17:16:02 -0400

    Formatting new PM class to flake8 compatibility

--- photonmover/instruments/Power_meters/ThorlabsPM400.py
+++ photonmover/instruments/Power_meters/ThorlabsPM400.py
@@ -1,18 +1,19 @@
 import sys
 import pyvisa as visa
-import numpy as np
 import time
-import csv
 from photonmover.Interfaces.Instrument import Instrument
 from photonmover.Interfaces.PowMeter import PowerMeter
 from enum import Enum
-sys.path.insert(0, '../..')
+
+sys.path.insert(0, "../..")
 
 
 MANUFACTURER_ID = 0x1313
 
+
 class PowerMeterTypes(Enum):
-    """ Container class for power meter model definitions """
+    """Container class for power meter model definitions"""
+
     PM400 = 0x8075
 
 
@@ -48,20 +49,19 @@ class ThorlabsPM400(Instrument, PowerMeter):
             print("No instrument address was provided, cannot initialize")
             return
         else:
-            print('Opening connnection to Thorlabs Power Meter PM400')
+            print("Opening connnection to Thorlabs Power Meter PM400")
 
             try:
-                self.instr = self.rm.open_resource(
-                    self.instr_address, timeout=10000)
+                self.instr = self.rm.open_resource(self.instr_address, timeout=10000)
                 self.is_initialized = True
             except ConnectionError:
-                raise ConnectionError('Cannot connect to Thorlabs Power Meter PM400')
-        
-        self.instr.read_termination = '\n'
+                raise ConnectionError("Cannot connect to Thorlabs Power Meter PM400")
+
+        self.instr.read_termination = "\n"
         self._initialize_params()
 
     def close(self):
-        print('Disconnecting Thorlabs Power Meter PM400')
+        print("Disconnecting Thorlabs Power Meter PM400")
         self.instr.close()
 
     def find_address(self):
@@ -70,13 +70,13 @@ class ThorlabsPM400(Instrument, PowerMeter):
         If only one address exists, it automatically applies that address to
         the the invoking class instance. This overrides provided addresses.
         """
-        model_string = ' '
+        model_string = " "
         for spec in PowerMeterTypes:
-            model_string += '(VI_ATTR_MODEL_CODE==0x{:04X}) || '.format(
-                spec.value)
-            model_string = model_string.rstrip(' || ')
+            model_string += "(VI_ATTR_MODEL_CODE==0x{:04X}) || ".format(spec.value)
+            model_string = model_string.rstrip(" || ")
             search_string = "USB?*?{{VI_ATTR_MANF_ID==0x{:04X} && ({})}}".format(
-                MANUFACTURER_ID, model_string)
+                MANUFACTURER_ID, model_string
+            )
 
         try:
             instr_list = self.rm.list_resources(search_string)
@@ -84,7 +84,9 @@ class ThorlabsPM400(Instrument, PowerMeter):
             instr_list = []
             raise Warning("No connected signal generators were found")
 
-        if len(instr_list) == 1:  # Only one relevant signal generator found, set as address
+        if (
+            len(instr_list) == 1
+        ):  # Only one relevant signal generator found, set as address
             print("Only found one applicable instrument, using this address.")
             print("Instrument address: {}".format(instr_list[0]))
             self._set_address(instr_address=instr_list[0])
@@ -100,16 +102,17 @@ class ThorlabsPM400(Instrument, PowerMeter):
 
     def _initialize_params(self):
         """
-        Set default parameters and options. 
+        Set default parameters and options.
         """
-        init_commands = ["*RST",                        # Reset to defaults
-                        "SENSE:POWER:UNIT DBM",         # Set dBm as default return unit
-                        "SENSe:CORRection:WAVelength 1348",     # Choose a default wavelength 
-                        "SENSE:CORRECTION:COLLECT:ZERO:STATE 0",# Do not use background zeroing
-                        "CONFIGURE:POWER",              # Return scaled power
-                        ]
-        
-        for cmd in init_commands: 
+        init_commands = [
+            "*RST",  # Reset to defaults
+            "SENSE:POWER:UNIT DBM",  # Set dBm as default return unit
+            "SENSe:CORRection:WAVelength 1348",  # Choose a default wavelength
+            "SENSE:CORRECTION:COLLECT:ZERO:STATE 0",  # Do not use background zeroing
+            "CONFIGURE:POWER",  # Return scaled power
+        ]
+
+        for cmd in init_commands:
             self.instr.write(cmd)
             time.sleep(0.2)
 
@@ -118,25 +121,29 @@ class ThorlabsPM400(Instrument, PowerMeter):
         Set the correction wavelength to the specified value (in nm)
         :return:
         """
-        self.instr.write("SENSe:CORRection:WAVelength {:d}".format(wavelength) )
+        self.instr.write("SENSe:CORRection:WAVelength {:d}".format(wavelength))
 
-    def set_power_unit(self, unit='W'):
+    def set_power_unit(self, unit="W"):
         """
-        Choose which unit to measure power in. Can be 'W' for watts or 
+        Choose which unit to measure power in. Can be 'W' for watts or
         'DBM' for dBm.
         """
-        if unit.lower() in ['w', 'dbm']:
-            self.instr.write("SENSE:POWER:UNIT {:s}".format(unit))  # Set dBm as default return unit
-        else: 
-            return ValueError("Invalid input given for power meter unit. Must be 'W' or 'DBM' ")
-        
+        if unit.lower() in ["w", "dbm"]:
+            self.instr.write(
+                "SENSE:POWER:UNIT {:s}".format(unit)
+            )  # Set dBm as default return unit
+        else:
+            return ValueError(
+                "Invalid input given for power meter unit. Must be 'W' or 'DBM' "
+            )
+
     def get_powers(self):
         """
         Returns the measured power, in system units (modified with `set_power_unit`)
-        :return: 
+        :return:
         """
-        pwr = float(self.instr.query("READ?")) 
-        return pwr 
+        pwr = float(self.instr.query("READ?"))
+        return pwr
 
     def set_range(self, channel, range):
         """
@@ -147,15 +154,9 @@ class ThorlabsPM400(Instrument, PowerMeter):
 
     def get_id(self):
         return self.instr.query("*IDN?")
-    
-    def close(self):
-        """
-        Close VISA connection. 
-        """
-        self.instr.close()
 
-if __name__ == '__main__':
 
+if __name__ == "__main__":
     pm = ThorlabsPM400()
     addresses = pm.find_address()
     pm.initialize(override_address=addresses[0])
