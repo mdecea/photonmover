729c5480cfaee7183459b87c834ea16bf1ccd315
Gavin West <westgn@mit.edu>
2023-08-10 17:02:21 -0400

    Formatting frequency counter class for flake8 consistency

--- photonmover/Interfaces/FrequencyCounter.py
+++ photonmover/Interfaces/FrequencyCounter.py
@@ -1,30 +1,29 @@
 # This is an interface for instruments which count cycles of a tone or return "instantaneous" frequency.
-
 from abc import ABC, abstractmethod
+
 
 class FrequencyCounter(ABC):
 
     def __init__(self):
         super().__init__()
 
-    @abstractmethod 
+    @abstractmethod
     def initialize(self):
         """
-        Initializes the instrument 
-        """
-        pass 
-    
-    @abstractmethod
-    def get_frequency(self, freq):
-        """
-        Sets the desired signal output frequency.
-        :param freq: frequency of the waveform (Hz)
+        Initializes the instrument
         """
         pass
 
-    @abstractmethod 
+    @abstractmethod
+    def get_frequency(self):
+        """
+        Gets the current measured frequency
+        """
+        pass
+
+    @abstractmethod
     def close(self):
-        """ 
+        """
         Closes the instrument connection
         """
-        pass 
+        pass
--- photonmover/instruments/frequency_counters/Keysight53220A.py
+++ photonmover/instruments/frequency_counters/Keysight53220A.py
@@ -1,59 +1,56 @@
-import sys
-import time
 import pyvisa as visa
 from photonmover.Interfaces.FrequencyCounter import FrequencyCounter
 from photonmover.Interfaces.Instrument import Instrument
 
 
-
 class Keysight53220A(Instrument, FrequencyCounter):
     """
-    Code for controlling thorlabs tabletop SOA/BOAs through VISA/pyserial. 
+    Code for controlling thorlabs tabletop SOA/BOAs through VISA/pyserial.
     These are typically model numbers S7FC or S9FC.
     """
 
     def __init__(
-            self,
-            gpib_address = "GPIB1::4::INSTR",
-        ):
+        self,
+        gpib_address="GPIB1::4::INSTR",
+    ):
         super().__init__()
 
         self.gpib_address = gpib_address
         self.instr = None
-
 
     def initialize(self):
         """
         Initializes the instrumen t
         :return:
         """
-        print('Opening connnection to Keysight Frequency Counter')
+        print("Opening connnection to Keysight Frequency Counter")
 
         rm = visa.ResourceManager()
         try:
             self.instr = rm.open_resource(
-                self.gpib_address, timeout=6000)  # 2 min timeout
-            print('Connected to Keysight frequency counter')
+                self.gpib_address, timeout=6000
+            )  # 2 min timeout
+            print("Connected to Keysight frequency counter")
         except BaseException:
-            raise ValueError('Cannot connect to the Keysight frequency counter')
-        
+            raise ValueError("Cannot connect to the Keysight frequency counter")
+
     def close(self):
-        print('Disconnecting Keysight frequency counter')
+        print("Disconnecting Keysight frequency counter")
         self.instr.close()
-    
+
     def identify(self):
         """
         Identify the instrument
         """
         return self.query("*IDN?")
 
-    def get_frequency(self, channel=[1,2], res=1e-1, f0=10e6):
-        """ 
-        Set measurement parameters and immediately trigger a frequency measurement 
-        on the specified channel(s). 
+    def get_frequency(self, channel=[1, 2], res=1e-1, f0=10e6):
+        """
+        Set measurement parameters and immediately trigger a frequency measurement
+        on the specified channel(s).
         Data is read from the instrument.
 
-        Measurements on the two channels are NOT concurrent. 
+        Measurements on the two channels are NOT concurrent.
 
         * **channel** (int):    Channel to make measurement on. Can be 1, 2, or [1,2].
         * **res** (float):      Frequency resolution, in Hz.
@@ -61,19 +58,20 @@ class Keysight53220A(Instrument, FrequencyCounter):
         """
 
         freq = []
-        if not hasattr(channel, '__iter__'):
-            channel=[channel]
-        
+        if not hasattr(channel, "__iter__"):
+            channel = [channel]
+
         for chan in channel:
-            self.instr.write("MEAS:FREQ? {:s}, {:s}, (@{:d})".format(str(f0), str(res), chan))
+            self.instr.write(
+                "MEAS:FREQ? {:s}, {:s}, (@{:d})".format(str(f0), str(res), chan)
+            )
             meas = self.instr.read()
-            freq.append( float(meas.strip("\n")) )
-        
+            freq.append(float(meas.strip("\n")))
+
         return freq
-        
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     counter = Keysight53220A()
     counter.initialize()
     counter.identify()
